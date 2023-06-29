e6eb4a72e6bdba6d43583e7949bd9ba7135a51c0
shafu.eth <selfouly@gmail.com>
2023-06-29 13:47:10 +0200

    mint multiple

--- test/DNft.t.sol
+++ test/DNft.t.sol
@@ -2,9 +2,10 @@
 pragma solidity =0.8.17;
 
 import "forge-std/console.sol";
+import "forge-std/Test.sol";
 import { DNft } from "../src/DNft.sol";
 
-contract DNftsTest {
+contract DNftsTest is Test {
   DNft dNft;
 
   function setUp() public {
@@ -15,6 +16,16 @@ contract DNftsTest {
   function test_mintNft() public {
     dNft.mintNft{value: 0.1 ether}(address(this));
   }
+  function test_mintNft_multiple() public {
+    dNft.mintNft{value: 0.1 ether}(address(this));
+    dNft.mintNft{value: 0.101 ether}(address(this));
+    dNft.mintNft{value: 0.102 ether}(address(this));
+  }
+  function testCannot_mintNft_insufficientFunds() public {
+    vm.expectRevert();
+    dNft.mintNft{value: 0.09 ether}(address(this));
+  }
+
 
   function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
     return 0x150b7a02;
