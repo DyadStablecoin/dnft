992fd1deb64c6b12992f72b52d314586d1a8a1de
shafu.eth <selfouly@gmail.com>
2023-07-20 11:19:19 +0300

    mainnet deploy

---
+++ script/Deploy.Mainnet.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "forge-std/Script.sol";
+import "../src/DNft.sol";
+
+contract DeployMainnet is Script {
+  function run() public {
+    vm.startBroadcast();
+    new DNft();
+    vm.stopBroadcast();
+  }
+}
+
