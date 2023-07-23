46e62394f4d7258b19be22f1665e4b97be98620c
shafu.eth <selfouly@gmail.com>
2023-07-23 16:00:03 +0200

    voted for/against

--- src/ISLL.sol
+++ src/ISLL.sol
@@ -2,4 +2,7 @@
 pragma solidity =0.8.17;
 
 interface ISLL {
+  error OnlyOwner();
+  error AlreadyVotedFor();
+  error AlreadyVotedAgainst();
 }
--- src/SLL.sol
+++ src/SLL.sol
@@ -2,7 +2,37 @@
 pragma solidity =0.8.17;
 
 import {ISLL} from "./ISLL.sol";
+import {DNft} from "./DNft.sol";
 
 contract SLL is ISLL {
+  uint public constant THRESHOLD = 5;
+
+  DNft public immutable dNft;
+
+  mapping (address => uint256) public votes; // vault   => votes
+  mapping (uint    => bool)    public voted; // DNft id => voted
+
+  constructor(DNft _dNft) {
+    dNft = _dNft;
+  }
+
+  function voteFor(uint id, address vault) external {
+    if (dNft.ownerOf(id) != msg.sender) { revert OnlyOwner(); }
+    if (voted[id])                      { revert AlreadyVotedFor(); }
+    voted[id]     = true;
+    votes[vault] += 1;
+  }
+
+  function voteAgainst(uint id, address vault) external {
+    if (dNft.ownerOf(id) != msg.sender) { revert OnlyOwner(); }
+    if (!voted[id])                     { revert AlreadyVotedAgainst(); }
+    voted[id]     = false;
+    votes[vault] -= 1;
+  }
+
+  function isValid(address vault) external view returns (bool) {
+    if (votes[vault] / dNft.totalSupply() > THRESHOLD) { return true; }
+    return false;
+  }
 
 }
