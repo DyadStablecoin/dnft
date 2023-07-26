c4cec15882b26f282211017c4e525ddc3ecade05
shafu.eth <selfouly@gmail.com>
2023-07-26 17:49:31 +0200

    add vault

--- src/SLL.sol
+++ src/SLL.sol
@@ -5,12 +5,16 @@ import {ISLL} from "./ISLL.sol";
 import {DNft} from "./DNft.sol";
 import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
 
+// SLL := Social Licensing Layer
 contract SLL is ISLL {
   using FixedPointMathLib for uint; 
 
-  uint public constant THRESHOLD = 200000000000000000; // 20%
-
   DNft public immutable dNft;
+
+  uint public constant THRESHOLD  = 200000000000000000; // 20%
+  uint public constant MAX_VAULTS = 5;
+
+  address[] public licensedVaults; // Length is always < MAX_VAULTS
 
   mapping (address => uint256) public votes; // vault   => votes
   mapping (uint    => bool)    public voted; // DNft id => voted
@@ -35,8 +39,32 @@ contract SLL is ISLL {
     return votes[vault].divWadDown(dNft.totalSupply());
   }
 
-  function hasEnoughVotes(address vault) external view returns (bool) {
+  function hasEnoughVotes(address vault) public view returns (bool) {
     if (countVotes(vault) > THRESHOLD) { return true; }
+    return false;
+  }
+
+  function add(address vault) external returns (bool) {
+    if (!hasEnoughVotes(vault)) return false;
+    if (licensedVaults.length < MAX_VAULTS) {
+      licensedVaults.push(vault); 
+      return true;
+    }
+
+    uint indexOfLeastVotes = 0;
+    uint leastVotes        = type(uint256).max;
+
+    for (uint i = 0; i < MAX_VAULTS; i++) {
+      if (countVotes(licensedVaults[i]) < leastVotes) {
+        indexOfLeastVotes = i;
+      }
+    }
+
+    if (countVotes(vault) > leastVotes) {
+      licensedVaults[indexOfLeastVotes] = vault;
+      return true;
+    }
+
     return false;
   }
 }
