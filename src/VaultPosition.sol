95f2e8435b415bfe9f14fcab7bc5ae4e31b95e7e
shafu.eth <selfouly@gmail.com>
2023-07-27 23:54:22 +0200

    Vault position

---
+++ src/VaultPosition.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import {IVaultPosition} from "./interfaces/IVaultPosition.sol";
+
+import {ERC721} from "@solmate/src/tokens/ERC721.sol";
+import {ERC4626} from "@solmate/src/mixins/ERC4626.sol";
+
+contract Vault is IVaultPosition, ERC721 {
+  constructor(
+    string memory name, 
+    string memory symbol 
+  ) ERC721(name, symbol) {}
+
+  function tokenURI(uint256 tokenId) 
+    public 
+    pure 
+    override 
+    returns (string memory) {
+      return string(abi.encodePacked(tokenId));
+  }
+}
