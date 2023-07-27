d4553fe90605b93386afb44961ee72551a6cdbad
shafu.eth <selfouly@gmail.com>
2023-07-27 21:38:47 +0200

    oracle

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,25 +3,51 @@ pragma solidity =0.8.17;
 
 import {DNft} from "./DNft.sol";
 import {IVault} from "./interfaces/IVault.sol";
+import {IAggregatorV3} from "./interfaces/IAggregatorV3.sol";
 
 import {ERC4626} from "@solmate/src/mixins/ERC4626.sol";
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
+import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 contract Vault is IVault, ERC4626 {
-  DNft public immutable dNft;
+  using FixedPointMathLib for uint;
+  using SafeCast          for int;
+
+  IAggregatorV3 public immutable oracle;
+  DNft          public immutable dNft;
 
   mapping (uint => uint) public xp; // id => xp
 
   constructor(
-      DNft  _dNft,
-      ERC20 _asset,
+      DNft          _dNft,
+      IAggregatorV3 _oracle,
+      ERC20         _asset,
       string memory _name,
       string memory _symbol
   ) ERC4626(_asset, _name, _symbol) {
-      dNft = _dNft;
+      dNft   = _dNft;
+      oracle = _oracle;
   }
 
   function totalAssets() public view override returns (uint) {
     return asset.balanceOf(address(this));
+  }
+
+  // collateral price in USD
+  function _collatPrice() 
+    private 
+    view 
+    returns (uint) {
+      (
+        uint80 roundID,
+        int256 price,
+        , 
+        uint256 timeStamp, 
+        uint80 answeredInRound
+      ) = oracle.latestRoundData();
+      if (timeStamp == 0)            revert IncompleteRound();
+      if (answeredInRound < roundID) revert StaleData();
+      return price.toUint256();
   }
 }
--- src/interfaces/IVault.sol
+++ src/interfaces/IVault.sol
@@ -2,5 +2,6 @@
 pragma solidity =0.8.17;
 
 interface IVault {
-
+  error StaleData      ();
+  error IncompleteRound();
 }
