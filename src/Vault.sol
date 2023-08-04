14a589a6890bdb9dff4b85ce480a796bf5e4aecb
shafu.eth <selfouly@gmail.com>
2023-08-04 15:31:08 +0200

    mint

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,17 +2,19 @@
 pragma solidity =0.8.17;
 
 import {DNft} from "./DNft.sol";
+import {Staking} from "./Staking.sol";
 import {IVault} from "./interfaces/IVault.sol";
 import {IAggregatorV3} from "./interfaces/IAggregatorV3.sol";
 
 import {ERC4626} from "@solmate/src/mixins/ERC4626.sol";
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
+import {Owned} from "@solmate/src/auth/Owned.sol";
 import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
 import {SafeTransferLib} from "@solmate/src/utils/SafeTransferLib.sol";
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 import {SafeTransferLib} from "@solmate/src/utils/SafeTransferLib.sol";
 
-contract Vault is IVault, ERC4626 {
+contract Vault is IVault, Owned, ERC4626 {
   using SafeCast        for int;
   using SafeTransferLib for ERC20;
 
@@ -21,11 +23,14 @@ contract Vault is IVault, ERC4626 {
 
   constructor(
       DNft          _dNft,
+      Staking       _staking, 
       IAggregatorV3 _oracle,
       ERC20         _asset,
       string memory _name,
       string memory _symbol
-  ) ERC4626(_asset, _name, _symbol) {
+  ) ERC4626(_asset, _name, _symbol)
+    Owned(address(_staking))
+  {
       dNft   = _dNft;
       oracle = _oracle;
   }
@@ -163,6 +168,15 @@ contract Vault is IVault, ERC4626 {
     override 
     returns (uint) {
       return asset.balanceOf(address(this));
+  }
+
+  function mint(
+    address to,
+    uint    amount
+  )
+    external
+    onlyOwner {
+      super._mint(to, amount);
   }
 
   // collateral price in USD
