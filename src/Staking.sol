e6d1de14d3a20e144dc3fb1dca1e1a067bc945aa
shafu.eth <selfouly@gmail.com>
2023-08-04 23:07:06 +0200

    staking

--- src/Staking.sol
+++ src/Staking.sol
@@ -1,8 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
+import {DNft} from "./DNft.sol";
+import {Vault} from "./Vault.sol";
 import {IStaking} from "./interfaces/IStaking.sol";
 
-contract Staking is IStaking {
+import {Owned} from "@solmate/src/auth/Owned.sol";
 
+contract Staking is IStaking, Owned {
+  DNft  public immutable dNft;
+  Vault public vault;
+
+  constructor(DNft _dNft) Owned(msg.sender) {
+    dNft = _dNft;
+  }
+
+  function setVault(
+    address vault
+  ) external
+    onlyOwner {
+      vault = vault;
+  }
+
+  function stake(
+      uint id, 
+      uint amount
+  ) external {
+      // TODO
+  }
+
+  function unstake(
+      uint id, 
+      uint amount
+  ) external {
+      // TODO
+  }
 }
