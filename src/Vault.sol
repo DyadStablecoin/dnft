7ecb526bf5cafe18cf2e77c98d9b6a7f4dbf815d
shafu.eth <selfouly@gmail.com>
2023-08-05 18:37:38 +0200

    total usd value

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.17;
 
 import {DNft} from "./DNft.sol";
 import {Staking} from "./Staking.sol";
+import {VaultManager} from "./VaultManager.sol";
 import {IVault} from "./interfaces/IVault.sol";
 import {IAggregatorV3} from "./interfaces/IAggregatorV3.sol";
 
@@ -19,20 +20,61 @@ contract Vault is IVault, Owned, ERC4626 {
   using SafeTransferLib for ERC20;
 
   DNft          public immutable dNft;
+  VaultManager  public immutable vaultManager;
   IAggregatorV3 public immutable oracle;
 
   constructor(
       DNft          _dNft,
-      Staking       _staking, 
+      VaultManager  _vaultManager,
       IAggregatorV3 _oracle,
+      Staking       _staking, 
       ERC20         _asset,
       string memory _name,
       string memory _symbol
   ) ERC4626(_asset, _name, _symbol)
     Owned(address(_staking))
   {
-      dNft   = _dNft;
-      oracle = _oracle;
+      dNft         = _dNft;
+      vaultManager = _vaultManager;
+      oracle       = _oracle;
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                        CUSTOM VAULT METHODS
+  //////////////////////////////////////////////////////////////*/
+  function liquidate(
+      uint id
+  ) external {
+      for (uint i = 0; i < vaultManager.getNumberOfVaults(id); i++) {
+        address vault = vaultManager.vaults(id, i);
+        uint usdVaule = balanceOf[address(uint160(id))] * _collatPrice();
+      }
+  }
+
+  function mint(
+    address to,
+    uint    amount
+  )
+    external
+    onlyOwner {
+      super._mint(to, amount);
+  }
+
+  // collateral price in USD
+  function _collatPrice() 
+    private 
+    view 
+    returns (uint) {
+      (
+        uint80 roundID,
+        int price,
+        , 
+        uint timeStamp, 
+        uint80 answeredInRound
+      ) = oracle.latestRoundData();
+      if (timeStamp == 0)            revert IncompleteRound();
+      if (answeredInRound < roundID) revert StaleData();
+      return price.toUint256();
   }
 
   /*//////////////////////////////////////////////////////////////
@@ -168,31 +210,5 @@ contract Vault is IVault, Owned, ERC4626 {
     override 
     returns (uint) {
       return asset.balanceOf(address(this));
-  }
-
-  function mint(
-    address to,
-    uint    amount
-  )
-    external
-    onlyOwner {
-      super._mint(to, amount);
-  }
-
-  // collateral price in USD
-  function _collatPrice() 
-    private 
-    view 
-    returns (uint) {
-      (
-        uint80 roundID,
-        int price,
-        , 
-        uint timeStamp, 
-        uint80 answeredInRound
-      ) = oracle.latestRoundData();
-      if (timeStamp == 0)            revert IncompleteRound();
-      if (answeredInRound < roundID) revert StaleData();
-      return price.toUint256();
   }
 }
--- src/VaultManager.sol
+++ src/VaultManager.sol
@@ -20,29 +20,44 @@ contract VaultManager is IVaultManager {
     sll = _sll;
   }
 
-  function add(uint id, address vault) external {
-    if (dNft.ownerOf(id)  != msg.sender) revert OnlyOwner(); 
-    if (vaults[id].length  > MAX_VAULTS) revert TooManyVaults();
-    if (!sll.isLicensed(vault))          revert VaultNotLicensed();
-    vaults[id].push(vault);
-    isDNftVault[id][vault] = true;
-    vaultsIndex[id][vault] = vaults[id].length - 1;
-    emit Added(id, vault);
+  function add(
+      uint id,
+      address vault
+  ) external {
+      if (dNft.ownerOf(id)  != msg.sender) revert OnlyOwner(); 
+      if (vaults[id].length  > MAX_VAULTS) revert TooManyVaults();
+      if (!sll.isLicensed(vault))          revert VaultNotLicensed();
+      vaults[id].push(vault);
+      isDNftVault[id][vault] = true;
+      vaultsIndex[id][vault] = vaults[id].length - 1;
+      emit Added(id, vault);
   }
 
-  function remove(uint id, address vault) external {
-    if (dNft.ownerOf(id)  != msg.sender) revert OnlyOwner();
-    if (!isDNftVault[id][vault])         revert NotDNftVault();
-    uint index        = vaultsIndex[id][vault];
-    uint vaultsLength = vaults[id].length;
-    address oldVault  = vaults[id][index];
-    for (uint i = index; i < vaultsLength - 1; ) {
-      vaults[i] = vaults[i+1];
-      unchecked { i++; }
-    }
-    vaults[id].pop();
-    isDNftVault[id][oldVault] = false;
-    vaultsIndex[id][oldVault] = 0; 
-    emit Removed(id, oldVault);
+  function remove(
+      uint id,
+      address vault
+  ) external {
+      if (dNft.ownerOf(id)  != msg.sender) revert OnlyOwner();
+      if (!isDNftVault[id][vault])         revert NotDNftVault();
+      uint index        = vaultsIndex[id][vault];
+      uint vaultsLength = vaults[id].length;
+      address oldVault  = vaults[id][index];
+      for (uint i = index; i < vaultsLength - 1; ) {
+        vaults[i] = vaults[i+1];
+        unchecked { i++; }
+      }
+      vaults[id].pop();
+      isDNftVault[id][oldVault] = false;
+      vaultsIndex[id][oldVault] = 0; 
+      emit Removed(id, oldVault);
+  }
+
+  function getNumberOfVaults(
+      uint id
+  ) 
+    external 
+    view 
+    returns (uint) {
+      return vaults[id].length;
   }
 }
