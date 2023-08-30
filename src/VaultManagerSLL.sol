ad7c4add7897fd50f1761ed1100f2833d505a988
shafu.eth <selfouly@gmail.com>
2023-08-30 12:49:05 +0200

    comment

--- src/VaultManagerSLL.sol
+++ src/VaultManagerSLL.sol
@@ -1,13 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import {ISLL} from "./interfaces/ISLL.sol";
 import {DNft} from "./DNft.sol";
-import {Dyad} from "./DYAD.sol";
-import {VaultManager} from "./VaultManager.sol";
 import {SLL} from "./SLL.sol";
-
-import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
 
 // SLL := Social Licensing Layer for Vault Managers
 contract VaultManagerSLL is SLL {
--- src/VaultSLL.sol
+++ src/VaultSLL.sol
@@ -4,9 +4,7 @@ pragma solidity =0.8.17;
 import {DNft} from "./DNft.sol";
 import {SLL} from "./SLL.sol";
 
-import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
-
-// SLL := Social Licensing Layer for Vault Managers
+// SLL := Social Licensing Layer for Vaults
 contract VaultSLL is SLL {
   constructor(DNft _dNft) SLL(_dNft, 60e16, 40e16) {}
 }
