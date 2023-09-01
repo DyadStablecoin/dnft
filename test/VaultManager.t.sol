116b51da026a926f8e4bf2281af101599d680ed9
shafu <selfouly@gmail.com>
2023-09-01 10:49:55 +0200

    refactor add

--- test/VaultManager.t.sol
+++ test/VaultManager.t.sol
@@ -28,14 +28,18 @@ contract VaultManagerTest is Test {
     dyad = new Dyad(vaultManagerSLL);
     vaultManager = new VaultManager(dNft, vaultSLL, dyad);
   }
+
+  function add(uint id, address vault) public {
+    vaultSLL.vote(id, vault);
+    vaultSLL.license(vault);
+    vaultManager.add(id, vault);
+  }
   
   ///////////////////////////
   // add
   function test_add() public {
     uint id = dNft.mintNft{value: 1 ether}(address(this));
-    vaultSLL.vote(id, RANDOM_VAULT_1);
-    vaultSLL.license(RANDOM_VAULT_1);
-    vaultManager.add(id, RANDOM_VAULT_1);
+    add(id, RANDOM_VAULT_1);
     assertEq(vaultManager.isDNftVault(id, RANDOM_VAULT_1), true);
     assertEq(vaultManager.vaults(id, 0), RANDOM_VAULT_1);
     assertEq(vaultManager.getVaultsCount(id), 1);
@@ -45,12 +49,8 @@ contract VaultManagerTest is Test {
 
   function test_addTwoVaults() public {
     uint id = dNft.mintNft{value: 1 ether}(address(this));
-    vaultSLL.vote(id, RANDOM_VAULT_1);
-    vaultSLL.vote(id, RANDOM_VAULT_2);
-    vaultSLL.license(RANDOM_VAULT_1);
-    vaultSLL.license(RANDOM_VAULT_2);
-    vaultManager.add(id, RANDOM_VAULT_1);
-    vaultManager.add(id, RANDOM_VAULT_2);
+    add(id, RANDOM_VAULT_1);
+    add(id, RANDOM_VAULT_2);
     assertEq(vaultManager.isDNftVault(id, RANDOM_VAULT_1), true);
     assertEq(vaultManager.isDNftVault(id, RANDOM_VAULT_2), true);
     assertEq(vaultManager.vaults(id, 0), RANDOM_VAULT_1);
@@ -64,9 +64,7 @@ contract VaultManagerTest is Test {
   // remove
   function test_remove() public {
     uint id = dNft.mintNft{value: 1 ether}(address(this));
-    vaultSLL.vote(id, RANDOM_VAULT_1);
-    vaultSLL.license(RANDOM_VAULT_1);
-    vaultManager.add(id, RANDOM_VAULT_1);
+    add(id, RANDOM_VAULT_1);
     vaultManager.remove(id, 0);
     assertEq(vaultManager.isDNftVault(id, RANDOM_VAULT_1), false);
     assertEq(vaultManager.getVaultsCount(id), 0);
@@ -76,15 +74,9 @@ contract VaultManagerTest is Test {
 
   function test_removeThreeVaults() public {
     uint id = dNft.mintNft{value: 1 ether}(address(this));
-    vaultSLL.vote(id, RANDOM_VAULT_1);
-    vaultSLL.vote(id, RANDOM_VAULT_2);
-    vaultSLL.vote(id, RANDOM_VAULT_3);
-    vaultSLL.license(RANDOM_VAULT_1);
-    vaultSLL.license(RANDOM_VAULT_2);
-    vaultSLL.license(RANDOM_VAULT_3);
-    vaultManager.add(id, RANDOM_VAULT_1);
-    vaultManager.add(id, RANDOM_VAULT_2);
-    vaultManager.add(id, RANDOM_VAULT_3);
+    add(id, RANDOM_VAULT_1);
+    add(id, RANDOM_VAULT_2);
+    add(id, RANDOM_VAULT_3);
     vaultManager.remove(id, 0);
     assertEq(vaultManager.getVaultsCount(id), 2);
     vaultManager.remove(id, 0);
@@ -96,7 +88,8 @@ contract VaultManagerTest is Test {
   ///////////////////////////
   // collatRatio
   function test_collatRatio() public {
-
+    uint id = dNft.mintNft{value: 1 ether}(address(this));
+    add(id, RANDOM_VAULT_1);
   }
 
   ///////////////////////////
