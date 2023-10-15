ed8f233d94bf6eeef01e8bac0145cfdf41372ce9
shafu.eth <selfouly@gmail.com>
2023-10-15 09:38:27 +0300

    new staking calculations

--- src/Staking.sol
+++ src/Staking.sol
@@ -11,6 +11,8 @@ import {FixedPointMathLib} from "@solmate/src/utils/FixedPointMathLib.sol";
 contract Staking is IStaking, Owned {
   using FixedPointMathLib for uint;
 
+  uint constant public RATE_INCREASE = 1e16;   // 0.01% 
+
   uint constant public REWARD_RATE = 8e15;   // 0.8% 
   uint constant public TIME_BONUS  = 135e12; // 0.135%
 
@@ -20,7 +22,7 @@ contract Staking is IStaking, Owned {
 
   mapping(uint => uint) public rewards;
   mapping(uint => uint) public balanceOf;
-  mapping(uint => uint) public lastUpdated;
+  mapping(uint => uint) public lastUpdated; // block number
 
   constructor(DNft _dNft) Owned(msg.sender) {
     dNft = _dNft;
@@ -64,33 +66,27 @@ contract Staking is IStaking, Owned {
   function _updateReward(
       uint id
   ) internal {
-      // uint initialStakeSize;
-      // uint timeSinceLastUpdate;
-      // uint sizeOfLastUpdate;
-      // uint reward = initialStakeSize + (initialStakeSize * 0.01 * timeSinceLastUpdate) + sizeOfLastUpdate;
+      uint lastUpdate = lastUpdated[id] == 0 ? block.number : lastUpdated[id];
 
       uint reward = _calcReward(
-          id, 
-          balanceOf[id], 
-          block.timestamp - lastUpdated[id]
+        id, 
+        lastUpdate
       );
       rewards[id] += reward;
+
+      lastUpdated[id] = block.number;
   }
 
   function _calcReward(
       uint id,
-      uint amount, 
-      uint duration
+      uint time
   ) internal
     view
     returns (uint) {
-      uint shares      = vault.balanceOf(address(uint160(id)));
-      uint sharesTotal = vault.totalSupply();
-      // =1/(1+(B5/B6))
-      uint equityControl = uint(1).divWadDown(1+shares.divWadDown(sharesTotal)); 
-      // B4*(B3^(1+B2))*B1*B8
-      return duration.rpow(1+TIME_BONUS, amount)
-        .mulWadDown(REWARD_RATE)
-        .mulWadDown(equityControl);
+      uint shares     = vault.balanceOf(address(uint160(id)));
+      uint rewardRate = time;
+      uint urp        = shares.mulWadDown(rewardRate).mulWadDown(REWARD_RATE);
+      uint decay      = urp.mulWadDown(REWARD_RATE.mulWadDown(urp.divWadDown(shares)));
+      return urp - decay;
   }
 }
