bf45f11ce6266e1bf474c481c6c8262df6cf3c12
shafu.eth <selfouly@gmail.com>
2023-10-27 20:42:26 +0200

    Idyad

--- src/Dyad.sol
+++ src/Dyad.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
+import {IDyad} from "./interfaces/IDyad.sol";
 import {SLL} from "./SLL.sol";
 import {ERC20} from "@solmate/src/tokens/ERC20.sol";
 
-contract Dyad is ERC20("DYAD Stable", "DYAD", 18) {
+contract Dyad is ERC20("DYAD Stable", "DYAD", 18), IDyad {
   SLL public immutable sll;
 
   error NotLicensed();
 
-  // manager => (dNft id => minted dyad)
+  // vault manager => (dNft id => minted dyad)
   mapping (address => mapping (uint => uint)) public mintedDyad; 
 
   constructor(SLL _sll) { sll = _sll; }
 
+  /// @inheritdoc IDyad
   function mint(
       uint    id, // we trust the caller to use an id that exists
       address to,
@@ -24,6 +26,7 @@ contract Dyad is ERC20("DYAD Stable", "DYAD", 18) {
       _mint(to, amount);
   }
 
+  /// @inheritdoc IDyad
   function burn(
       uint    id, // we trust the caller to use an id that exists
       address from,
