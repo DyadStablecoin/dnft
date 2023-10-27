ec228f50790db8d9fc0fa58fc9e55cd1e3a54ae4
shafu.eth <selfouly@gmail.com>
2023-10-27 21:48:56 +0200

    refactor

--- test/Dyad.t.sol
+++ test/Dyad.t.sol
@@ -7,11 +7,20 @@ import { BaseTest } from "./BaseTest.sol";
 
 contract DyadTest is BaseTest {
 
+  uint amount = 420;
+
   function test_mint() public {
     uint id = dNft.mintNft{value: 0.1 ether}(address(this));
 
     vm.prank(address(vaultManager));
-    dyad.mint(id, address(this), 420);
+    dyad.mint(id, address(this), amount);
+  }
+
+  function testFail_vaultManagerNotLicensedForMint() public {
+    uint id = dNft.mintNft{value: 0.1 ether}(address(this));
+
+    dyad.mint(id, address(this), amount);
+    assertEq(dyad.mintedDyad(address(vaultManager), id), amount);
   }
 
   function test_burn() public {
@@ -19,10 +28,17 @@ contract DyadTest is BaseTest {
 
     // mint
     vm.prank(address(vaultManager));
-    dyad.mint(id, address(this), 420);
+    dyad.mint(id, address(this), amount);
 
     // burn
     vm.prank(address(vaultManager));
-    dyad.burn(id, address(this), 420);
+    dyad.burn(id, address(this), amount);
+
+    assertEq(dyad.mintedDyad(address(vaultManager), id), 0);
+  }
+
+  function testFail_vaultManagerNotLicensedForBurn() public {
+    uint id = dNft.mintNft{value: 0.1 ether}(address(this));
+    dyad.burn(id, address(this), amount);
   }
 }
