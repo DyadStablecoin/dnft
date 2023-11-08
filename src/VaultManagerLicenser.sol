5cdd0a98d68a5c3a92f2e5dc46e18755d0662e99
shafu.eth <selfouly@gmail.com>
2023-11-08 13:30:35 +0100

    licenser

---
+++ src/VaultManagerLicenser.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import {DNft} from "./DNft.sol";
+import {Licenser} from "./SLL.sol";
+
+contract VaultManagerLicenser is Licenser {
+  constructor() {}
+}
