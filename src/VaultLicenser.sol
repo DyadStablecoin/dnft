594092b6b00704aca66a3c53a11c81db16b68714
shafu.eth <selfouly@gmail.com>
2023-11-08 14:58:00 +0100

    rm Licenser subs

--- src/VaultLicenser.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.8.17;
-
-import {DNft} from "./DNft.sol";
-import {Licenser} from "./Licenser.sol";
-
-contract VaultLicenser is Licenser {
-  constructor() {}
-}
